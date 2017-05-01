43d16302f8c3ac886e63f2dd0c85bd00578521f2
Kusanagi <kusanagi@Kusanagi-no-MacBook-Pro.local>
2017-05-01 11:57:17 +0900

    kusanagi

--- FacebookLogin.php
+++ FacebookLogin.php
@@ -1,93 +1,96 @@
 <?php
 /*
+ * facebookのソーシャルログイン実装時のクラス
  * 動作確認していない
- *
- * Facebookログインで使える関数の詰め合わせ
  * */
 
-define("APP_ID", "");
-define("APP_SECRET", "");
-define("STATE", "");
-define("CALLBACK_URL", "");
+class Facebook{
+	const APP_ID = "";
+	const APP_SECRET = "";
+	const STATE = "";
+	const CALLBACK_URL = "";
 
-//facebookのログイン画面にリダイレクト
-function redirect(){
-	$callback_url = urlencode(CALLBACK_URL);
-	$facebook_redirct_url =  "https://www.facebook.com/dialog/oauth?client_id=". APP_ID. "&redirect_uri=". $callback_url. "&state=". STATE. "&scope=". $scope;
-	header("Location: {$facebook_redirct_url}");
-	return; // or exit();
-}
-
-//フェイスブックとの認証やユーザーの登録
-function oauth(){
-	if(isCodeCorrect($_GET) == false) return;
-	if(isStateCorrect($_GET) == false) return;
-	
-	$token_array = accessToken($code, CALLBACK_URL);
-	if($token_array == null) return;
-
-	$access_token = $token_array['access_token'];
-	$user_array = userGet($access_token);
-	if($user_array == null) return;
-
-	userCreate($user_array);
-	return;
-}
-
-//codeが正しくきているのか？
-function isCodeCorrect($params){
-	//codeのついて確認
-	if(isset($params['code'])){
-		return true;
-	}else if(isset($params['error_reason'])){
-		echo $params['error_reason'];
-		return false;
-	}else{
-		echo "なぜここにきた？";
-		return false;
+	//facebookのログイン画面のリダイレクトURL取得
+	public function redirectURL(){
+		$callback_url = urlencode(self::CALLBACK_URL);
+		$facebook_redirct_url =  "https://www.facebook.com/dialog/oauth?client_id=". self::APP_ID. "&redirect_uri=". $callback_url. "&state=". self::STATE. "&scope=". $scope;
+		return $facebool_url;
 	}
 
-}
+	//フェイスブックとの認証やユーザーの登録
+	public function oauth($params){
+		if($this->isCodeCorrect($params) == false) return false;
+		if($this->isStateCorrect($params) == false) return false;
+		
+		$token_array = $this->accessToken($code);
+		if($token_array == null) return false;
 
-//facebookに渡したstateが正しいかチェック
-function isStateCorrect($params){
-	if(isset($params['state'])){
-		$state = str_replace('#_=_', '', $params['state']);
-		if($state == STATE){
+		$access_token = $token_array['access_token'];
+		$user_array = $this->userGet($access_token);
+		if($user_array == null) return;
+
+		$result = $this->userCreate($user_array);
+		return $result;
+	}
+
+	//codeが正しくきているのか？
+	private function isCodeCorrect($params){
+		//codeのついて確認
+		if(isset($params['code'])){
 			return true;
+		}else if(isset($params['error_reason'])){
+			echo $params['error_reason'];
+			return false;
 		}else{
+			echo "Unexpected error";
 			return false;
 		}
-	}else{
-		return false;
-	}	
-}
 
-//アクセストークンの取得
-function accessToken($code, $redirect_url){
-	$get_token_url = "https://graph.facebook.com/v2.7/oauth/access_token?client_id=". APP_ID. "&redirect_uri=". $redirect_url. "&client_secret=". APP_SECRET. "&code=${code}";
-	if($get_token_json = file_get_contents($get_token_url)){
-		$get_token_array = json_decode($get_token_json, true);
-		return $get_token_array;
-	}else{
-		return null;	
 	}
-}
 
-//ユーザー情報の取得だけ
-function userGet($access_token){
-	$info_url = "https://graph.facebook.com/v2.7/me?fields=name,first_name,last_name,link&access_token=${access_token}";
-	if($json = file_get_contents($info_url)){
-		$user_array = json_decode($json, true);
-		return $user_array;
-	}else{
-		return null;	
+	//facebookに渡したstateが正しいかチェック
+	private function isStateCorrect($params){
+		if(isset($params['state'])){
+			$state = str_replace('#_=_', '', $params['state']);
+			if($state == self::STATE){
+				return true;
+			}else{
+				echo "Unexpected error";
+				return false;
+			}
+		}else{
+			return false;
+		}	
 	}
-}
 
-//ユーザー情報の登録
-function userCreate($user_array){
-	//DB登録処理
-	return;
-}
+	//アクセストークンの取得
+	private function accessToken($code){
+		$get_token_url = "https://graph.facebook.com/v2.7/oauth/access_token?client_id=". self::APP_ID. "&redirect_uri=". self::CALLBACK_URL. "&client_secret=". self::APP_SECRET. "&code=${code}";
+		if($get_token_json = file_get_contents($get_token_url)){
+			$get_token_array = json_decode($get_token_json, true);
+			return $get_token_array;
+		}else{
+			return null;	
+		}
+	}
 
+	//ユーザー情報の取得だけ
+	private function userGet($access_token){
+		$info_url = "https://graph.facebook.com/v2.7/me?fields=name,first_name,last_name,link&access_token=${access_token}";
+		if($json = file_get_contents($info_url)){
+			$user_array = json_decode($json, true);
+			return $user_array;
+		}else{
+			return null;	
+		}
+	}
+
+	//ユーザー情報の登録
+	private function userCreate($user_array){
+		//DB登録処理
+		return true; //or false
+	}
+
+
+
+}
--- TwitterLogin.php
+++ TwitterLogin.php
@@ -5,64 +5,62 @@
 /*php用のtwitterSDKを入れておいてね！*/
 require('twitteroauth/autoload.php');
 use Abraham\TwitterOAuth\TwitterOAuth;
-use Phalcon\Mvc\Controller;
 
-define('API_KEY', '');
-define('API_SECRET', '');
+class Twitter{
+	const API_KEY = '';
+	const API_SECRET = '';
+	const CALLBACK_URL = "";
 
-//twitterログイン画面に移動
-function redirect(){
-	$callback_url = "";
-	$connection = new TwitterOAuth(API_KEY, API_SECRET);
-	$request_token = $connection->oauth("oauth/request_token", array('oauth_callback' => $redirect_url));
-	$twitter_login_url = 'https://api.twitter.com/oauth/authenticate?oauth_token='. $request_token['oauth_token'];
-	header("Location: ${twitter_login_url}");
-	return; //or exit();
-}
-
-
-function oauth(){
-	if(isTokenCorrect($_GET) == false) return;
-	$oauth_verifier = $_GET['oauth_verifier'];
-	$oauth_token = $_GET['oauth_token'];
-
-
-	$access_token = accessToken($oauth_token, $oauth_verifier);
-	$user_info = userGet($access_token['oauth_token'], $access_token['oauth_token_secret']);
-
-	userCreate($user_info);
-}
-
-//tokenが正しく送られてきているかのチェック
-function isTokenCorrect($params){
-	if(isset($params['oauth_token']) && isset($params['oauth_varifier'])){
-		return true;
-	}else if(isset($params['denied'])){
-		//拒否の場合はdeniedを取得
-		echo $params['denied'];
-		return false;
-	}else{
-		echo "なぜここにきた？";
-		return false;
+	//twitterログイン画面に移動するURL取得
+	public function redirect(){
+		$connection = new TwitterOAuth(self::API_KEY, self::API_SECRET);
+		$request_token = $connection->oauth("oauth/request_token", array('oauth_callback' => self::CALLBACK_URL));
+		$twitter_login_url = 'https://api.twitter.com/oauth/authenticate?oauth_token='. $request_token['oauth_token'];
+		return $twitter_login_url;
 	}
-}
-
-//twitterからアクセストークンの取得
-function accessToken($oauth_token, $oauth_verifier){
-	$connection = new TwitterOAuth(API_KEY, API_SECRET, $oauth_token, $oauth_verifier);
-	$access_token = $connection->oauth('oauth/access_token', array('oauth_verifier' => $oauth_verifier, 'oauth_token' => $oauth_token));
-	return $access_token;
-}
 
 
-//ユーザー情報の取得
-function userGet($oauth_token, $oauth_token_secret){
-	$user_connection = new TwitterOAuth(API_KEY, API_SECRET, $oauth_token, $oauth_token_secret);
-	$user_info = $user_connection->get('account/verify_credentials');
-	return $user_info;
-}
+	public function oauth($params){
+		if($this->isTokenCorrect($params) == false) return false;
+		$oauth_verifier = $params['oauth_verifier'];
+		$oauth_token = $params['oauth_token'];
+
+		$access_token = $this->accessToken($oauth_token, $oauth_verifier);
+		$user_info = $this->userGet($access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+		$this->userCreate($user_info);
+	}
+
+	//tokenが正しく送られてきているかのチェック
+	private function isTokenCorrect($params){
+		if(isset($params['oauth_token']) && isset($params['oauth_varifier'])){
+			return true;
+		}else if(isset($params['denied'])){
+			//拒否の場合はdeniedを取得
+			echo $params['denied'];
+			return false;
+		}else{
+			echo "UNexpected error";
+			return false;
+		}
+	}
+
+	//twitterからアクセストークンの取得
+	private function accessToken($oauth_token, $oauth_verifier){
+		$connection = new TwitterOAuth(self::API_KEY, self::API_SECRET, $oauth_token, $oauth_verifier);
+		$access_token = $connection->oauth('oauth/access_token', array('oauth_verifier' => $oauth_verifier, 'oauth_token' => $oauth_token));
+		return $access_token;
+	}
 
 
-function userCreate($user_info){
-	//DBに入れる
+	//ユーザー情報の取得
+	private function userGet($oauth_token, $oauth_token_secret){
+		$user_connection = new TwitterOAuth(self::API_KEY, self::API_SECRET, $oauth_token, $oauth_token_secret);
+		$user_info = $user_connection->get('account/verify_credentials');
+		return $user_info;
+	}
+
+	private function userCreate($user_info){
+		//DBに入れる
+	}
 }
